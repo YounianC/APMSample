1db6a4acce0e05b024801aab421f5fe4880d4273
Younianlu <379412437@qq.com>
2018-02-26 11:44:31 +0800

    添加测试用Tomcat项目

---
+++ APMAgentTestTomcat/src/main/java/Controller/IndexController.java
@@ -0,0 +1,20 @@
+package Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@Controller
+@ResponseBody
+public class IndexController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String redis(HttpServletRequest request, Map<String, Object> model) {
+        return "index";
+    }
+
+}

1381c2d3bc7ae371b87b67874223c3220b00a020
Younianlu <379412437@qq.com>
2018-02-27 14:24:39 +0800

    tomcat测试添加jedis

--- APMAgentTestTomcat/src/main/java/Controller/IndexController.java
+++ APMAgentTestTomcat/src/main/java/Controller/IndexController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import redis.clients.jedis.Jedis;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
@@ -14,6 +15,10 @@ public class IndexController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(HttpServletRequest request, Map<String, Object> model) {
+        Jedis jedis = new Jedis("localhost");
+        System.out.println("服务正在运行: " + jedis.ping());
+        jedis.set("key", "" + System.currentTimeMillis());
+        System.out.println("value of key:" + jedis.get("key"));
         return "index";
     }
 

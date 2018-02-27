c5e5936c132274a7fbb7d58f946cab428da126f0
Younianlu <379412437@qq.com>
2018-02-27 14:25:24 +0800

    更新 jedis 插件

--- APMAgent/src/main/java/younian/apmsample/agent/plugin/jedis/JedisInterceptor.java
+++ APMAgent/src/main/java/younian/apmsample/agent/plugin/jedis/JedisInterceptor.java
@@ -5,12 +5,12 @@ import younian.apmsample.agent.intercept.ClassInstanceMethodInterceptor;
 public class JedisInterceptor extends ClassInstanceMethodInterceptor {
     @Override
     protected void before() {
-        System.out.println("TestInterceptor before..");
+        System.out.println("JedisInterceptor before..");
     }
 
     @Override
     protected Object after(Object result) {
-        System.out.println("TestInterceptor after..");
+        System.out.println("JedisInterceptor after..");
         return result;
     }
 }

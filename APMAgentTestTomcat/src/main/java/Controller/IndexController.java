7b2f8a6ee9ee0401f670330feeb8caa0033a8c78
Younianlu <379412437@qq.com>
2018-03-13 15:10:41 +0800

    添加mysql测试

--- APMAgentTestTomcat/src/main/java/Controller/IndexController.java
+++ APMAgentTestTomcat/src/main/java/Controller/IndexController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import redis.clients.jedis.Jedis;
 
 import javax.servlet.http.HttpServletRequest;
+import java.sql.*;
 import java.util.Map;
 
 @Controller
@@ -31,6 +32,40 @@ public class IndexController {
     private void test1(Jedis jedis){
         jedis.set("key", "" + System.currentTimeMillis());
         System.out.println("value of key:" + jedis.get("key"));
+
+        testMysql();
     }
 
+    public static void testMysql() {
+        Connection con = null;
+        Statement stmt = null;
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/blog", "root", "root");
+            stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from `t_admin`");
+            while (rs.next()) {
+                long id = rs.getLong("id");
+            }
+
+            int r = stmt.executeUpdate("update `t_admin` set username = 'name' where id = 1");
+
+            PreparedStatement pstmt = con.prepareStatement("update `t_admin` set username = ? where id = 1");
+            pstmt.setString(1, "admin");
+            int rowcount = pstmt.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException ignored) {
+            }
+        }
+    }
 }

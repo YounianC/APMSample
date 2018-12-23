1be80369e181bcb70d28dfb67241a3e78dcbbcf4
YOUNIAN_WIN10\Administrator <youlian584521@gmail.com>
2018-12-23 14:44:01 +0800

    server根据traceId调整

--- APMServer/src/main/java/Controller/IndexController.java
+++ APMServer/src/main/java/Controller/IndexController.java
@@ -8,15 +8,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.*;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 @Controller
 public class IndexController {
 
-    private static TreeMap<Integer, JSONObject> spanMap = new TreeMap<>(new Comparator<Integer>() {
+    private static TreeMap<String, Map<Integer, JSONObject>> spanMap = new TreeMap<>(new Comparator<String>() {
         @Override
-        public int compare(Integer o1, Integer o2) {
-            return o1 - o2;
+        public int compare(String o1, String o2) {
+            return o1.compareTo(o2);
         }
     });
 
@@ -29,31 +32,35 @@ public class IndexController {
     @ResponseBody
     @RequestMapping(value = "/getData", method = RequestMethod.GET)
     public String getData(HttpServletRequest request, Map<String, Object> model) {
-        TreeMap<Integer, JSONObject> tmpMap = new TreeMap<>(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return o1 - o2;
-            }
-        });
-        for (Map.Entry<Integer, JSONObject> entry : spanMap.entrySet()) {
-            tmpMap.put(entry.getKey(),JSONObject.parseObject(entry.getValue().toJSONString()));
-        }
-        ///////copy end
-
         JSONArray roots = new JSONArray();
-        for (JSONObject jsonObject : tmpMap.values()) {
-            int parent = jsonObject.getIntValue("parentSpanId");
 
-            JSONObject parentObj = tmpMap.get(parent);
-            if (parentObj != null && parent != jsonObject.getIntValue("spanId")) {
-                if (!parentObj.containsKey("children")) {
-                    parentObj.put("children", new JSONArray());
+        for (Map.Entry<String, Map<Integer, JSONObject>> entry : spanMap.entrySet()) {
+            TreeMap<Integer, JSONObject> tmpMap = new TreeMap<>(new Comparator<Integer>() {
+                @Override
+                public int compare(Integer o1, Integer o2) {
+                    return o1 - o2;
                 }
-                parentObj.getJSONArray("children").add(jsonObject);
-            }
+            });
 
-            if (jsonObject.getIntValue("parentSpanId") == 0) {
-                roots.add(jsonObject);
+            for (Map.Entry<Integer, JSONObject> span : entry.getValue().entrySet()) {
+                tmpMap.put(span.getKey(), JSONObject.parseObject(span.getValue().toJSONString()));
+            }
+            ///////copy end
+
+            for (JSONObject jsonObject : tmpMap.values()) {
+                int parent = jsonObject.getIntValue("parentSpanId");
+
+                JSONObject parentObj = tmpMap.get(parent);
+                if (parentObj != null && parent != jsonObject.getIntValue("spanId")) {
+                    if (!parentObj.containsKey("children")) {
+                        parentObj.put("children", new JSONArray());
+                    }
+                    parentObj.getJSONArray("children").add(jsonObject);
+                }
+
+                if (jsonObject.getIntValue("parentSpanId") == 0) {
+                    roots.add(jsonObject);
+                }
             }
         }
 
@@ -64,7 +71,14 @@ public class IndexController {
     @RequestMapping(value = "/uploadSpan", method = RequestMethod.POST)
     public void uploadSpan(HttpServletRequest request) {
         JSONObject object = JSONObject.parseObject(request.getParameter("span"));
-        spanMap.put(object.getInteger("spanId"), object);
+
+        String traceId = object.getString("traceId");
+        if (!spanMap.containsKey(traceId)) {
+            Map<Integer, JSONObject> map = new HashMap<>();
+            spanMap.put(traceId, map);
+        }
+
+        spanMap.get(traceId).put(object.getInteger("spanId"), object);
         System.out.println(object);
     }
 
